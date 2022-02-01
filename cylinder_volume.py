b6976d38ddc48fc0187b60a106966024eafddb3b
Sajan-acharya29 <98785043+Sajan-acharya29@users.noreply.github.com>
2022-01-31 20:58:26 -0500

    Add files via upload

---
+++ cylinder_volume.py
@@ -0,0 +1,11 @@
+import math
+r = float(input("enter cylinder's radius : "))
+h = float(input("enter cylinder's height: "))
+
+volume = math.pi * (r**2) * h # volume of the cylinder is calculated by pi * r^2 * h
+
+print("The volume of the cylinder is: ", (round((volume), 2))) # using round function to take the final answer with 2 decimal places as necessary for volume.
+
+
+
+
